4af130947da600e68972a8091a16c907fe8f5cbe
A0120680r <A0120680r@BOR3902.iss.nus.edu.sg>
2015-08-12 02:16:57 -0400

    commit messs

---
+++ src/java/com/acme/advweb/day5/LogoutServlet.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acme.advweb.day5;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.invalidate();
+        
+        resp.sendRedirect("login.html");
+    }
+    
+    
+    
+}
